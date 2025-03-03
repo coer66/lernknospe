f0c65b33d656d2236a3a284b0e447be10dd4dea2
coer66 <oer.almeida@web.de>
2025-03-03 11:31:00 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,11 +1,13 @@
 function cookieOk() {
   var now = new Date(); // Variable für aktuelles Datum
   var lifetime = now.getTime(); // Variable für Millisekunden seit 1970 bis aktuelles Datum
-  var deleteCookie = lifetime + 2592000000; // Macht den Cookie 30 Tage gültig.
+  var deleteCookie = lifetime + 86400; // Macht den Cookie 1 Tage gültig.
      
   now.setTime(deleteCookie);
   var enddate = now.toUTCString();
      
   document.cookie = "setCookieHinweis = set; path=/; secure; expires=" + enddate;
   document.getElementById("cookie-popup").classList.add("hidden");
+}
+function cookienOk() {
 }
