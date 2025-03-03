b024ded795d43f02ac37307c36343248c2800c72
coer66 <oer.almeida@web.de>
2025-03-03 10:19:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php if($showPopup) { ?>
+  <div id="cookie-popup">
+    <div class="hinweis">
+      <p>Wir verwenden Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.</p>
+    </div> 
+    <span class="more">
+      <a href="datenschutz.php">Details</a>
+    </span>
+    <button onclick='cookieOk()'>OK, ich bin einverstanden.</button>
+  </div>
+<?php  }; ?>
