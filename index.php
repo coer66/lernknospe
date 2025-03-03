0d021c5edd2eef1613c881ad42ee69239905d692
coer66 <oer.almeida@web.de>
2025-03-03 11:03:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
 <?php if($showPopup) { ?>
   <div id="cookie-popup">
     <div class="hinweis">
-      <p>Wir verwenden Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.</p>
+      <p>Diese Seite verwendet keine Cookies. Wir können dennoch nicht ausschliessen, dass der Provider technisch notwendige Cookies einfordert. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung zu.</p>
     </div> 
     <span class="more">
-      <a href="datenschutz.php">Details</a>
+      <a href="dsvgo.html">Details</a>
     </span>
     <button onclick='cookieOk()'>OK, ich bin einverstanden.</button>
   </div>
